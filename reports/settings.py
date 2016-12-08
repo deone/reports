b6baa726df08cd0d7dc7a704490b3afe4ca79323
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-08 17:24:28 +0000

    - Comment out database setting since we'll be using PyMongo - Create app.

--- reports/settings.py
+++ reports/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,12 @@ WSGI_APPLICATION = 'reports.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+} """
 
 
 # Password validation
