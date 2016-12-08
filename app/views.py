7c8f084479342180a3d10f092d174d287515305a
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-08 18:55:49 +0000

    Add timestamp to vendor objects before inserting.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.http import JsonResponse
+from django.utils import timezone
 
 from utils import get_collection
 
@@ -8,6 +9,9 @@ import requests
 def index(request):
     response = requests.get(settings.VEND_VENDORS_URL).json()
     vendors = response['results']
+
+    # Add timestamp to vendor objects
+    [vendor.update({'date': timezone.now()}) for vendor in vendors]
 
     vendor_collection = get_collection('vendors')
     result = vendor_collection.insert_many(vendors)
