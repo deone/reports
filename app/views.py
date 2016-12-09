3b5a8f56377b4e9abb69eaa06196cb728bd8000b
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-09 17:31:16 +0000

    Add more key/value pairs.

--- app/views.py
+++ app/views.py
@@ -11,31 +11,53 @@ def index(request):
     vendors = response['results']['vendors']
     voucher_values = response['results']['voucher_values']
 
-    # Add timestamp to vendor objects
+    # Add more key/value pairs to vendor objects
+
+    # Add date
     now = timezone.now()
     [vendor.update({'date': now}) for vendor in vendors]
 
-    vendor_collection = get_collection('vendors')
-    # result = vendor_collection.insert_many(vendors)
+    # Add total vend value
+    [vendor.update({
+        'total_vend_value': sum([vc['value'] * vc['count'] for vc in vendor['vend_count']])
+    }) for vendor in vendors]
+
+    # Add total vend count
+    [vendor.update({
+        'total_vend_count': sum([vc['count'] for vc in vendor['vend_count']])
+    }) for vendor in vendors]
     
     # Create CSV file
-    file_name = '%s_%s_%s_%s.%s' % ('Vend_Report', str(now.day), str(now.month), str(now.year), 'csv')
-    voucher_values_line = '%s%s%s' % ('Vendor Name,Vendor Company,', ','.join([str(x) for x in voucher_values]), '\n')
+    file_name = '%s_%s_%s_%s.%s' % (
+        'Vend_Report', str(now.day), str(now.month), str(now.year), 'csv'
+        )
+
+    header = '%s%s%s%s%s\n' % (
+        'Vendor Name,',
+        'Vendor Company,',
+        ','.join([str(value) for value in voucher_values]),
+        ',Total Vend Value',
+        ',Total Vend Count'
+        )
     
     with open(file_name, 'a') as f:
-        f.write(voucher_values_line)
+        f.write(header)
         for vendor in vendors:
             vend_count = vendor['vend_count']
-            vendor_report_line = '%s,%s,%s,%s,%s,%s,%s\n' % (
+            vend_count_string = ','.join([str(elem['count']) for elem in vend_count])
+            line = '%s,%s,%s,%s,%s\n' % (
                 vendor['name'],
                 vendor['company_name'],
-                vend_count[0]['1'],
-                vend_count[1]['2'],
-                vend_count[2]['5'],
-                vend_count[3]['10'],
-                vend_count[4]['20']
+                vend_count_string,
+                vendor['total_vend_value'],
+                vendor['total_vend_count']
                 )
-            f.write(vendor_report_line)
+
+            f.write(line)
+
+    # Insert into database
+    # vendor_collection = get_collection('vendors')
+    # result = vendor_collection.insert_many(vendors)
 
     # Send email
 
