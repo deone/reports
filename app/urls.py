5b00f061b3e78746a04cad029cb89e5aeb4bb73f
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-01-08 14:35:51 +0000

    Change URL format to /2016, /12-2016 and /31-12-2016 for year, month and day respectively.

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<service>[a-z]+)/(?P<year>[0-9]{4})$', views.get_report),
-    url(r'^(?P<service>[a-z]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]+)$', views.get_report),
-    url(r'^(?P<service>[a-z]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/(?P<day>[0-9]+)$', views.get_report),
-    url(r'^(?P<service>[a-z]+)/last-(?P<period>[0-9]+)-days$', views.get_report_by_date_range),
-    url(r'^(?P<service>[a-z]+)/(?P<_from>[0-9]{4}-[0-9]+-[0-9]+)/(?P<to>[0-9]{4}-[0-9]+-[0-9]+)$', views.get_report_by_date_range),
+    url(r'^(?P<service>[a-z]+)/(?P<month>[0-9]+-[0-9]{4})$', views.get_report),
+    url(r'^(?P<service>[a-z]+)/(?P<day>[0-9]+-[0-9]+-[0-9]{4})$', views.get_report),
+
+    # url(r'^(?P<service>[a-z]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]+)$', views.get_report),
+    # url(r'^(?P<service>[a-z]+)/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/(?P<day>[0-9]+)$', views.get_report),
+    # url(r'^(?P<service>[a-z]+)/last-(?P<days>[0-9]+)-days$', views.get_report_by_date_range),
+    # url(r'^(?P<service>[a-z]+)/(?P<_from>[0-9]{4}-[0-9]+-[0-9]+)/(?P<to>[0-9]{4}-[0-9]+-[0-9]+)$', views.get_report_by_date_range),
 ]
--- app/views.py
+++ app/views.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 
 from utils import create_report, send_report, order_date, order_stringify_date
 
+from datetime import timedelta
+from collections import OrderedDict
+
 def error_or_success_message(status_code, reason):
     if status_code != 200:
         return HttpResponseServerError(reason)
@@ -11,15 +14,39 @@ def error_or_success_message(status_code, reason):
         return JsonResponse({'status': 'ok'})
 
 def get_report(request, service, year=None, month=None, day=None):
-    date = order_date(year=year, month=month, day=day)
+    date = OrderedDict()
+    if year is not None:
+        date['year'] = int(year)
+    elif month is not None:
+        parts = month.split('-')
+        date['month'] = parts[0]
+        date['year'] = parts[1]
+    elif day is not None:
+        parts = day.split('-')
+        date['day'] = parts[0]
+        date['month'] = parts[1]
+        date['year'] = parts[2]
+
+    print date
 
     _file = create_report(request.get_host(), service, date)
     response = send_report(service, _file)
 
     return error_or_success_message(response.status_code, response.reason)
 
-def get_report_by_date_range(request, service, _from=None, to=None, period=None):
-    _file = create_report(request.get_host(), service, _from=order_stringify_date(_from), to=order_stringify_date(to))
+""" def get_report_by_date_range(request, service, _from=None, to=None, days=None):
+    print _from
+    if days is not None:
+        now = timezone.now()
+        start = now - timedelta(days=int(days))
+        _from = '%s-%s-%s' % (start.year, start.month, start.day)
+        to = '%s-%s-%s' % (now.year, now.month, now.day)
+    else:
+        _from = order_stringify_date(_from)
+        to = order_stringify_date(to)
+
+    print _from
+    _file = create_report(request.get_host(), service, _from=_from, to=to)
     response = send_report(service, _file)
 
-    return error_or_success_message(response.status_code, response.reason)
+    return error_or_success_message(response.status_code, response.reason) """
