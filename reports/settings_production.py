77386160ea82cb81b463f2afd128e8c5512e67fb
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-01-09 17:57:45 +0000

    Add more report recipients.

--- reports/settings_production.py
+++ reports/settings_production.py
@@ -13,3 +13,9 @@ VENDOR_VENDS_URL = VENDOR_URL + '/vends'
 
 # messaging
 MESSAGING_URL = 'http://' + IP + ':7710'
+TO = [
+    'alwaysdeone@gmail.com',
+    'georged@spectrawireless.com',
+    'sdarko@spectrawireless.com',
+    'cadzisu@spectrawireless.com'
+]
